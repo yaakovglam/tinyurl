8d9b53cb778c89e12b178a05c5a4157c0dc199a3
Your Name <your.email@example.com>
2024-12-03 15:46:35 +0200

    initial commit

---
+++ src/main/java/com/handson/tinyurl/TinyurlApplication.java
@@ -0,0 +1,12 @@
+package com.handson.tinyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyurlApplication.class, args);
+    }
+}
---
+++ src/main/java/com/handson/tinyurl/config/MongoConfig.java
@@ -0,0 +1,33 @@
+package com.handson.tinyurl.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "tinydb";
+    }
+
+    @Override
+    public boolean autoIndexCreation() {
+        return true;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString("host");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+
+}
